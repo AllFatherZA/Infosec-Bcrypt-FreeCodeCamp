f6c2b3ef2d6290efe363ccea070507c4dd8af3bc
SL MNYANDENI <29422396@adt.nwu.ac.za>
2025-04-20 23:14:32 +0200

    replacing syncronised functions

--- server.js
+++ server.js
@@ -12,18 +12,14 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    /*Store hash in your db*/
-    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-        /*res == true or false*/
-      });
-  });
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
 
 
 //END_ASYNC
 
 //START_SYNC
-
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 //END_SYNC
