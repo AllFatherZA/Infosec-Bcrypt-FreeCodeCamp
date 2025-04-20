12d6d1e5385ace977339c4450e4baa1127219722
SL MNYANDENI <29422396@adt.nwu.ac.za>
2025-04-20 22:38:51 +0200

    added hash and compare functions to server

--- server.js
+++ server.js
@@ -11,13 +11,17 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    /*Store hash in your db*/
+  });
 
 
 //END_ASYNC
 
 //START_SYNC
-
+bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    /*res == true or false*/
+  });
 
 
 //END_SYNC
